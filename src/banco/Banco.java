2f7e07e5ac58018a1f51b95054b3a3e2bdd1c022
alumno <alumno@DAM202>
2025-11-18 14:11:33 +0100

    reintegros

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -5,8 +5,8 @@ public class Banco {
 	public static void main(String[] args) {
 		Cuenta cuenta1= new Cuenta(1000.00);
 		
-		Cliente cl1= new Cliente(cuenta1, "Mar", 100.00, 0);
-		Cliente cl2= new Cliente(cuenta1, "Daniel", 200.00, 0);
+		Cliente cl1= new Cliente(cuenta1, "Mar", 50.00, 0);
+		Cliente cl2= new Cliente(cuenta1, "Daniel", 25.00, 0);
 		cl1.start();
 		cl2.start();
 	}
